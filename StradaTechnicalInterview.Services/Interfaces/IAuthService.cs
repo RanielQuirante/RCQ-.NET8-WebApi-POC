ba724e3dd76effe5730118f6445f132af5f1a155
Raniel Christian Quirante <raniel.quirante@arcodea.com>
2025-02-22 14:40:49 +0800

    - Implement jwt bearer token to endpoints.

---
+++ StradaTechnicalInterview.Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using StradaTechnicalInterview.Models.Application;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace StradaTechnicalInterview.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        JwtSecurityToken GenerateToken(LoginModel login);
+    }
+}
